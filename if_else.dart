8904f8584583ced62d4a7e59a253cabd333bfac6
rafiqi <muhammadrafiqi171@gmail.com>
2024-10-08 10:04:34 +0700

    Create if_else.dart

---
+++ if_else.dart
@@ -0,0 +1,11 @@
+//INI ADALAH CONTOH IF DAN ELSE
+void main(){
+  var nilaiTugas = 70;
+  var nilaiAbsen = 80;
+
+   if(nilaiTugas >= 75 && nilaiAbsen >= 75){
+    print('antum berhasil coyyy');
+   }else{
+       print('kasian mana masih muda');
+   }
+ }
